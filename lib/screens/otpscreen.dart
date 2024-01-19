45706777ea69797593ae136a163fdd589274a10b
Agarwal7088 <agarwal12ishu7088@gmail.com>
2024-01-19 14:14:19 +0530

    done

--- lib/screens/otpscreen.dart
+++ lib/screens/otpscreen.dart
@@ -1,83 +1,68 @@
-import 'package:cab_booking_user/screens/otpscreen.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:doorapp/auth/admin_auth/admin_phoneno_login.dart';
+// import 'package:doorapp/user_homescreen/user_homescreen.dart';
+import 'package:cab_booking_user/screens/home.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class UserPhoneNoLogin extends StatefulWidget {
-  UserPhoneNoLogin({Key? key});
+class UserOtp extends StatefulWidget {
+  const UserOtp({Key? key, required this.verificationId});
+  final String verificationId;
 
   @override
-  State<UserPhoneNoLogin> createState() => _UserPhoneNoLoginState();
+  State<UserOtp> createState() => _UserOtpState();
 }
 
-class _UserPhoneNoLoginState extends State<UserPhoneNoLogin> {
-  final TextEditingController _contactNumberController =
-      TextEditingController();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  String? _verificationId;
-  bool isLoading = false;
-  String? _errorText;
+class _UserOtpState extends State<UserOtp> {
+  TextEditingController otpController = TextEditingController();
+  bool isLoading = false; // Track the loading state
+  String? _errorText; // Error text to show if OTP is invalid
 
-  // Function to send OTP to the user's phone number
-  Future<void> _sendOTP() async {
+  void verifyOTP() async {
     setState(() {
-      isLoading = true;
+      isLoading = true; // Show loading indicator on button
     });
 
-    String phoneNumber = '+91${_contactNumberController.text.trim()}';
-    if (_contactNumberController.text.trim().length != 10) {
+    String otp = otpController.text.trim();
+    if (otp.isEmpty) {
       setState(() {
-        _errorText = 'Phone number should be exactly 10 digits.';
-        isLoading = false;
+        _errorText = 'Please enter OTP'; // Show error text if OTP is blank
+        isLoading = false; // Hide loading indicator on button
       });
       return;
     } else {
       setState(() {
-        _errorText = null;
+        _errorText = null; // Clear error text if OTP is not blank
       });
     }
+
+    PhoneAuthCredential credential = PhoneAuthProvider.credential(
+      verificationId: widget.verificationId,
+      smsCode: otp,
+    );
 
     try {
-      await _auth.verifyPhoneNumber(
-        phoneNumber: phoneNumber,
-        verificationCompleted: (PhoneAuthCredential credential) async {
-          await _auth.signInWithCredential(credential);
-          _navigateToUserOtp();
-        },
-        verificationFailed: (FirebaseAuthException e) {
-          print('Phone number verification failed: ${e.message}');
-        },
-        codeSent: (String verificationId, int? resendToken) {
-          setState(() {
-            _verificationId = verificationId;
-            isLoading = false;
-          });
-          _navigateToUserOtp();
-        },
-        codeAutoRetrievalTimeout: (String verificationId) {
-          setState(() {
-            _verificationId = verificationId;
-            isLoading = false;
-          });
-        },
-      );
-    } catch (e) {
-      print('Error sending OTP: $e');
+      UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+      if (userCredential.user != null) {
+        // Navigate to the UserHomeScreen page after successful verification
+        Navigator.popUntil(context, (route) => route.isFirst);
+        Navigator.pushReplacement(
+          context,
+          CupertinoPageRoute(builder: (context) => HomeScreen()),
+        );
+      }
+    } on FirebaseAuthException catch (ex) {
       setState(() {
-        isLoading = false;
+        _errorText =
+            'Invalid OTP. Please try again.'; // Show error text for invalid OTP
+        isLoading = false; // Hide loading indicator on button
+      });
+    } finally {
+      setState(() {
+        isLoading = false; // Hide loading indicator on button
       });
     }
-  }
-
-  // Function to navigate to the UserOtp page
-  void _navigateToUserOtp() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) =>
-            UserOtp(verificationId: _verificationId.toString()),
-      ),
-    );
   }
 
   @override
@@ -100,14 +85,14 @@ class _UserPhoneNoLoginState extends State<UserPhoneNoLogin> {
               //   height: screenHeight * 0.1,
               // ),
               Text(
-                " User",
+                " Enter",
                 style: TextStyle(
                     fontSize: 60,
                     fontWeight: FontWeight.bold,
                     color: Colors.brown.shade900),
               ),
               Text(
-                " Login",
+                " OTP",
                 style: TextStyle(
                     fontSize: 60,
                     fontWeight: FontWeight.bold,
@@ -119,13 +104,13 @@ class _UserPhoneNoLoginState extends State<UserPhoneNoLogin> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextFormField(
-                  maxLength: 10,
                   keyboardType: TextInputType.phone,
-                  controller: _contactNumberController,
+                  maxLength: 6,
+                  controller: otpController,
                   cursorColor: Colors.brown.shade900,
                   decoration: InputDecoration(
                     counter: const Offstage(),
-                    labelText: 'Enter Phone No',
+                    labelText: 'Enter OTP',
                     errorText: _errorText, // Show error text if not null
                     labelStyle: TextStyle(color: Colors.brown.shade900),
                     enabledBorder: OutlineInputBorder(
@@ -148,7 +133,9 @@ class _UserPhoneNoLoginState extends State<UserPhoneNoLogin> {
                   margin: const EdgeInsets.all(10),
                   width: double.infinity,
                   child: ElevatedButton(
-                    onPressed: isLoading ? null : _sendOTP,
+                    onPressed: isLoading
+                        ? null
+                        : verifyOTP, // Disable the button when loading
                     style: ElevatedButton.styleFrom(
                       padding: const EdgeInsets.symmetric(
                           horizontal: 20.0, vertical: 7.0),
@@ -164,7 +151,7 @@ class _UserPhoneNoLoginState extends State<UserPhoneNoLogin> {
                             ),
                           )
                         : const Text(
-                            "Login",
+                            "Submit",
                             style: TextStyle(
                               color: Colors.white,
                               fontSize: 18,
@@ -177,15 +164,14 @@ class _UserPhoneNoLoginState extends State<UserPhoneNoLogin> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Text("Login as Admin?"),
+                  const Text("Login as Admin"),
                   TextButton(
                     child: Text(
                       'Admin',
                       style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20,
-                        color: Colors.brown.shade900,
-                      ),
+                          fontSize: 20,
+                          color: Colors.brown.shade900,
+                          fontWeight: FontWeight.bold),
                     ),
                     onPressed: () {
                       // Navigator.push(

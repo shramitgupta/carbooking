8cfab2ac7de6a449e917e6da8e2461b0e2b45dd0
shramitgupta <shramitgupta1212@gmail.com>
2024-04-09 16:15:25 +0530

    dd

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,36 @@
-import 'package:cab_booking_user/screens/home.dart';
-import 'package:cab_booking_user/screens/signup.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cab_booking_user/authentication/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  await Permission.locationWhenInUse.isDenied.then((valueOfPermission) {
+    if (valueOfPermission) {
+      Permission.locationWhenInUse.request();
+    }
+  });
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: (FirebaseAuth.instance.currentUser != null)
-          ? UserPhoneNoLogin()
-          // ? const HomeScreen()
-          : UserPhoneNoLogin(),
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: LoginScreen(),
     );
-    //     MaterialApp(
-    //   home: (FirebaseAuth.instance.currentUser != null)
-    //       ? const AdminHomeScreen()
-    //       : AdminPhoneNoLogin(),
-    // );
   }
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -3,9 +3,12 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  cloud_firestore
+  connectivity_plus
   firebase_auth
   firebase_core
+  firebase_storage
+  geolocator_windows
+  permission_handler_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
